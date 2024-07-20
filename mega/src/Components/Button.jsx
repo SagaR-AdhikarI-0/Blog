b595986618aa689b03fc67ee3ba48d6805869028
SagaR-AdhikarI-0 <sagaradhikari2027@gmail.com>
2024-07-20 10:13:57 +0545

    Duplicate deleted

--- mega/src/Components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({children,type='button',className='',...props}) {
-  return (
-    <button className={`p-2 rounded ${className}` } {...props}>{children}</button>
-  )
-}
-
-export default Button
--- mega/src/Components/Header/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Logo({className}) {
-  return (
-    <div className={`${className} font-bold `}>Logo</div>
-  )
-}
-
-export default Logo
